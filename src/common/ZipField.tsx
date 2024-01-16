25c2b07e7b8691448d737a0325b674e7e5cc2c92
Rich McGhee <rmcghee@stowers.org>
2024-01-16 14:58:27 -0600

    Get zip info from local db.

--- src/common/ZipField.tsx
+++ src/common/ZipField.tsx
@@ -11,12 +11,21 @@ export const ZipField: React.FC<ZipFieldProps> = ({
   ...validatedFieldProps
 }) => {
   const fetchZipData = async (zipCode: string) => {
+    const edgeFunction = 'http://127.0.0.1:54321/functions/v1/get-zip'
     // Need to validate here too, since the setter is always called after validation, even
     // if validation fails.
     if (validateZip(zipCode)) {
       console.log(`REceived ${zipCode}`);
 
-      
+      const response = await fetch(edgeFunction, {
+        method: 'POST',
+        body: JSON.stringify({ 'zip': zipCode }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (!response.ok) throw new Error('Network response was not ok');
+      const data = await response.json();
       // Implement the API request logic here
       // use onZipDataReceived(data); to set parent state
     }
